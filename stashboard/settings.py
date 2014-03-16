62a1af0e97885249547f89fca01fb6c4b2c464eb
Mark Janssen <mark@praseodym.net>
2014-03-16 18:04:01 +0100

    Beheer version

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,10 +2,20 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "CH Status"
+SITE_AUTHOR = "CH Beheer"
+SITE_URL = "https://beheerch.appspot.com"
+REPORT_URL = "mailto:beheer@ch.tudelft.nl"
+
+# Twitter update settings
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
+TWITTER_HANDLE = 'beheer'
+
+# RSS Feed settings
+RSS_NUM_EVENTS_TO_FETCH = 50
 
 # OAuth Consumer Credentials
 CONSUMER_KEY = 'anonymous'
